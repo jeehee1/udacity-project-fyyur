b0a81e9472f63261b28bfabb72af23196e28aa69
JiheeW <jeehee1w@gmail.com>
2022-01-25 11:52:06 +0000

    modify venue attribute in show_artist method

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -314,38 +314,57 @@ def show_artist(artist_id):
   # shows the artist page with the given artist_id
   # TODO: replace with real artist data from the artist table, using artist_id
 
-  artist = Artist.query.get(artist_id)
+  artist = Artist.query.get_or_404(artist_id)
 
-  past_shows = db.session.query(Show, Venue).filter(Show.artist_id==artist_id).filter(Show.venue_id==Venue.id).filter(Show.start_time<datetime.now()).all()
-  upcoming_shows = db.session.query(Show, Venue).filter(Show.artist_id==artist_id).filter(Show.venue_id==Venue.id).filter(Show.start_time>datetime.now()).all()
+  past_shows = []
+  upcoming_shows = []
+  # past_shows = db.session.query(Show, Venue).filter(Show.artist_id==artist_id).filter(Show.venue_id==Venue.id).filter(Show.start_time<datetime.now()).all()
+  # upcoming_shows = db.session.query(Show, Venue).filter(Show.artist_id==artist_id).filter(Show.venue_id==Venue.id).filter(Show.start_time>datetime.now()).all()
 
-  data = {
-    'id' : artist.id,
-    'name' : artist.name,
-    'genres' : artist.genres,
-    'city' : artist.city,
-    'state' : artist.state,
-    'phone' : artist.phone,
-    'website' : artist.website_link,
-    'facebook_link' : artist.facebook_link,
-    'seeking_venue' : artist.seeking_venue,
-    'seeking_description' : artist.seeking_description,
-    'image_link' : artist.image_link,
-    'past_shows' : [{
-      'venue_id' : venue.id,
-      'venue_name' : venue.name,
-      'venue_image_link' : venue.image_link,
-      'start_time' : show.start_time.strftime('%y-%m-%d %H:%M:%S')
-    }for show, venue in past_shows],
-    'upcoming_shows' : [{
-      'venue_id' : venue.id,
-      'venue_name' : venue.name,
-      'venue_image_link' : venue.image_link,
-      'start_time' : show.start_time.strftime('%y-%m-%d %H:%M:%S')
-    }for show, venue in upcoming_shows],
-    'past_shows_count' : len(past_shows),
-    'upcoming_shows_count' : len(upcoming_shows)
-  }
+  for artist_show in artist.show:
+    temp_show = {
+      'venue_id' : artist_show.venue_id,
+      'venue_name' : artist_show.venue.name,
+      'venue_image_link' : artist_show.venue.image_link,
+      'start_time' : artist_show.start_time.strftime('%y-%m-%d %H:%M')
+    }
+    if artist_show.start_time > datetime.now():
+      upcoming_shows.append(temp_show)
+    else:
+      past_shows.append(temp_show)
+    
+  data = vars(artist)
+  data['past_shows'] = past_shows
+  data['upcoming_shows'] = upcoming_shows
+  data['past_shows_count'] = len(past_shows)
+  data['upcoming_shows_count'] = len(upcoming_shows)
+  # data = {
+  #   'id' : artist.id,
+  #   'name' : artist.name,
+  #   'genres' : artist.genres,
+  #   'city' : artist.city,
+  #   'state' : artist.state,
+  #   'phone' : artist.phone,
+  #   'website' : artist.website_link,
+  #   'facebook_link' : artist.facebook_link,
+  #   'seeking_venue' : artist.seeking_venue,
+  #   'seeking_description' : artist.seeking_description,
+  #   'image_link' : artist.image_link,
+  #   'past_shows' : [{
+  #     'venue_id' : venue.id,
+  #     'venue_name' : venue.name,
+  #     'venue_image_link' : venue.image_link,
+  #     'start_time' : show.start_time.strftime('%y-%m-%d %H:%M:%S')
+  #   }for show, venue in past_shows],
+  #   'upcoming_shows' : [{
+  #     'venue_id' : venue.id,
+  #     'venue_name' : venue.name,
+  #     'venue_image_link' : venue.image_link,
+  #     'start_time' : show.start_time.strftime('%y-%m-%d %H:%M:%S')
+  #   }for show, venue in upcoming_shows],
+  #   'past_shows_count' : len(past_shows),
+  #   'upcoming_shows_count' : len(upcoming_shows)
+  # }
   
   return render_template('pages/show_artist.html', artist=data)
 
