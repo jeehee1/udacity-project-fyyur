0edc72f48d69b34b4861e08278db9c61b7654f09
JiheeW <jeehee1w@gmail.com>
2022-01-25 15:22:59 +0000

    add function to check the validation of form in create_artist_submission method

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -468,39 +468,42 @@ def create_artist_submission():
   # called upon submitting the new artist listing form
   # TODO: insert form data as a new Venue record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
-  form = ArtistForm(request.form)
-  error = False
-  try:
-    artist = Artist()
-    form.populate_obj(artist)
-    # artist = Artist(
-    #   name = form.name.data,
-    #   city = form.city.data,
-    #   state = form.state.data,
-    #   phone = form.phone.data,
-    #   genres = form.genres.data,
-    #   facebook_link = form.facebook_link.data,
-    #   image_link = form.image_link.data,
-    #   website_link = form.website_link.data,
-    #   seeking_venue = form.seeking_venue.data,
-    #   seeking_description = form.seeking_description.data,
-    # )
-    db.session.add(artist)
-    db.session.commit()
+  form = ArtistForm(request.form, meta={'csrf' : False})
+  if form.validate():
+    try:
+      artist = Artist()
+      form.populate_obj(artist)
+      # artist = Artist(
+      #   name = form.name.data,
+      #   city = form.city.data,
+      #   state = form.state.data,
+      #   phone = form.phone.data,
+      #   genres = form.genres.data,
+      #   facebook_link = form.facebook_link.data,
+      #   image_link = form.image_link.data,
+      #   website_link = form.website_link.data,
+      #   seeking_venue = form.seeking_venue.data,
+      #   seeking_description = form.seeking_description.data,
+      # )
+      db.session.add(artist)
+      db.session.commit()
 
-    # on successful db insert, flash success
-    flash('Artist ' + request.form['name'] + ' was successfully listed!')
+      # on successful db insert, flash success
+      flash('Artist ' + request.form['name'] + ' was successfully listed!')
 
-  except:
-    error = True
-    db.session.rollback()
-    flash('An error occurred. Artist ' + form.name + ' could not be listed.')  
-  
-  finally:
-    db.session.close()
+    except:
+      error = True
+      db.session.rollback()
+      flash('An error occurred. Artist ' + form.name + ' could not be listed.')  
     
+    finally:
+      db.session.close()
 
-
+  else:
+    message=[]
+    for field, err in form.errors.items():
+      message.append(field+' '+'|'.join(err))
+      flash('Errors '+str(message))
 
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
