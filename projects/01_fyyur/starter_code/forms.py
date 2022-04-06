1ae8f6a49d62fc2fdae1563438d163f6578e2640
JiheeW <jeehee1w@gmail.com>
2022-04-06 14:43:54 +0100

    create enums.py

--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -1,85 +1,84 @@
 from datetime import datetime
-from sre_parse import State
-from flask_wtf import FlaskForm, Form
+from flask_wtf import FlaskForm as Form
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
 from wtforms.validators import DataRequired, AnyOf, URL
 import re
 
-state_choices=[
-    ('AL', 'AL'),
-    ('AK', 'AK'),
-    ('AZ', 'AZ'),
-    ('AR', 'AR'),
-    ('CA', 'CA'),
-    ('CO', 'CO'),
-    ('CT', 'CT'),
-    ('DE', 'DE'),
-    ('DC', 'DC'),
-    ('FL', 'FL'),
-    ('GA', 'GA'),
-    ('HI', 'HI'),
-    ('ID', 'ID'),
-    ('IL', 'IL'),
-    ('IN', 'IN'),
-    ('IA', 'IA'),
-    ('KS', 'KS'),
-    ('KY', 'KY'),
-    ('LA', 'LA'),
-    ('ME', 'ME'),
-    ('MT', 'MT'),
-    ('NE', 'NE'),
-    ('NV', 'NV'),
-    ('NH', 'NH'),
-    ('NJ', 'NJ'),
-    ('NM', 'NM'),
-    ('NY', 'NY'),
-    ('NC', 'NC'),
-    ('ND', 'ND'),
-    ('OH', 'OH'),
-    ('OK', 'OK'),
-    ('OR', 'OR'),
-    ('MD', 'MD'),
-    ('MA', 'MA'),
-    ('MI', 'MI'),
-    ('MN', 'MN'),
-    ('MS', 'MS'),
-    ('MO', 'MO'),
-    ('PA', 'PA'),
-    ('RI', 'RI'),
-    ('SC', 'SC'),
-    ('SD', 'SD'),
-    ('TN', 'TN'),
-    ('TX', 'TX'),
-    ('UT', 'UT'),
-    ('VT', 'VT'),
-    ('VA', 'VA'),
-    ('WA', 'WA'),
-    ('WV', 'WV'),
-    ('WI', 'WI'),
-    ('WY', 'WY'),
-]
+# state_choices=[
+#     ('AL', 'AL'),
+#     ('AK', 'AK'),
+#     ('AZ', 'AZ'),
+#     ('AR', 'AR'),
+#     ('CA', 'CA'),
+#     ('CO', 'CO'),
+#     ('CT', 'CT'),
+#     ('DE', 'DE'),
+#     ('DC', 'DC'),
+#     ('FL', 'FL'),
+#     ('GA', 'GA'),
+#     ('HI', 'HI'),
+#     ('ID', 'ID'),
+#     ('IL', 'IL'),
+#     ('IN', 'IN'),
+#     ('IA', 'IA'),
+#     ('KS', 'KS'),
+#     ('KY', 'KY'),
+#     ('LA', 'LA'),
+#     ('ME', 'ME'),
+#     ('MT', 'MT'),
+#     ('NE', 'NE'),
+#     ('NV', 'NV'),
+#     ('NH', 'NH'),
+#     ('NJ', 'NJ'),
+#     ('NM', 'NM'),
+#     ('NY', 'NY'),
+#     ('NC', 'NC'),
+#     ('ND', 'ND'),
+#     ('OH', 'OH'),
+#     ('OK', 'OK'),
+#     ('OR', 'OR'),
+#     ('MD', 'MD'),
+#     ('MA', 'MA'),
+#     ('MI', 'MI'),
+#     ('MN', 'MN'),
+#     ('MS', 'MS'),
+#     ('MO', 'MO'),
+#     ('PA', 'PA'),
+#     ('RI', 'RI'),
+#     ('SC', 'SC'),
+#     ('SD', 'SD'),
+#     ('TN', 'TN'),
+#     ('TX', 'TX'),
+#     ('UT', 'UT'),
+#     ('VT', 'VT'),
+#     ('VA', 'VA'),
+#     ('WA', 'WA'),
+#     ('WV', 'WV'),
+#     ('WI', 'WI'),
+#     ('WY', 'WY'),
+# ]
 
-genres_choices=[
-    ('Alternative', 'Alternative'),
-    ('Blues', 'Blues'),
-    ('Classical', 'Classical'),
-    ('Country', 'Country'),
-    ('Electronic', 'Electronic'),
-    ('Folk', 'Folk'),
-    ('Funk', 'Funk'),
-    ('Hip-Hop', 'Hip-Hop'),
-    ('Heavy Metal', 'Heavy Metal'),
-    ('Instrumental', 'Instrumental'),
-    ('Jazz', 'Jazz'),
-    ('Musical Theatre', 'Musical Theatre'),
-    ('Pop', 'Pop'),
-    ('Punk', 'Punk'),
-    ('R&B', 'R&B'),
-    ('Reggae', 'Reggae'),
-    ('Rock n Roll', 'Rock n Roll'),
-    ('Soul', 'Soul'),
-    ('Other', 'Other'),
-]
+# genres_choices=[
+#     ('Alternative', 'Alternative'),
+#     ('Blues', 'Blues'),
+#     ('Classical', 'Classical'),
+#     ('Country', 'Country'),
+#     ('Electronic', 'Electronic'),
+#     ('Folk', 'Folk'),
+#     ('Funk', 'Funk'),
+#     ('Hip-Hop', 'Hip-Hop'),
+#     ('Heavy Metal', 'Heavy Metal'),
+#     ('Instrumental', 'Instrumental'),
+#     ('Jazz', 'Jazz'),
+#     ('Musical Theatre', 'Musical Theatre'),
+#     ('Pop', 'Pop'),
+#     ('Punk', 'Punk'),
+#     ('R&B', 'R&B'),
+#     ('Reggae', 'Reggae'),
+#     ('Rock n Roll', 'Rock n Roll'),
+#     ('Soul', 'Soul'),
+#     ('Other', 'Other'),
+# ]
 
 def is_valid_phone(number):
     """ Validate phone numbers like:
@@ -98,7 +97,7 @@ def is_valid_phone(number):
     regex = re.compile('^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$')
     return regex.match(number)
 
-class ShowForm(FlaskForm):
+class ShowForm(Form):
     def __init__(self, formdata=None, **kwargs):
         super().__init__(formdata, **kwargs)
 
@@ -170,13 +169,13 @@ class VenueForm(Form):
 
     def validate(self):
         """Define a custom validate method in your Form:"""
-        rv = FlaskForm.validate(self)
+        rv = Form.validate(self)
         if not rv:
             return False
         if not is_valid_phone(self.phone.data):
             self.phone.errors.append('Invalid phone.')
             return False
-        if not set(self.genres.data).issubset(dict(genres_choices()).keys()):
+        if not set(self.genres.data).issubset(dict(Genre.choices()).keys()):
             self.genres.errors.append('Invalid genres.')
             return False
         if self.state.data not in dict(State.choices()).keys():
